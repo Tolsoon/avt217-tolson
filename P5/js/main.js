9a968bf8dc4373911855f92929b7ae6f6a7972a7
Tolsoon <42841423+Tolsoon@users.noreply.github.com>
2020-04-25 23:13:21 -0400

    ?????

--- P5/js/main.js
+++ P5/js/main.js
@@ -1,29 +1,30 @@
 $(document).ready(function() {
-   var number1 = Math.floor(Math.random() * 3);
-   var number2 = Math.floor(Math.random() * 3);
-   var number3 = Math.floor(Math.random() * 3);
 
-   	console.log(number1);
-    console.log(number2);
-    console.log(number3);
+    lever();
 
-    var slotArray1 = [".seven", ".square", ".circle"];
-    var slotArray2 = [".seven2", ".square2", ".circle2"];
-    var slotArray3 = [".seven3", ".square3", ".circle3"];
+    $('.switch').click(function() {
 
-    console.log(slotArray1[number1]);
-    console.log(slotArray2[number2]);
-    console.log(slotArray3[number3]);
+      lever();
 
-    $(slotArray1[number1]).toggleClass('hidden');
-    $(slotArray2[number2]).toggleClass('hidden');
-    $(slotArray3[number3]).toggleClass('hidden');
-
-
-
-
-
-
+    });
 
 
 });
+
+
+
+function lever(){
+
+        var slotArray1 = [".seven", ".square" , ".circle"];
+        var slotArray2 = [".seven2", ".square2" , ".circle2"];
+        var slotArray3 = [".seven3", ".square3" , ".circle"];
+
+        var number1 = Math.floor(Math.random() * 3);
+        var number2 = Math.floor(Math.random() * 3);
+        var number3 = Math.floor(Math.random() * 3);
+
+        $(slotArray1[number1]).removeClass('hidden');
+        $(slotArray2[number2]).removeClass('hidden');
+        $(slotArray3[number3]).removeClass('hidden');
+
+      }
