cfb83be91a5144e5c42a384ee32dc0d74b196c4c
Tolsoon <42841423+Tolsoon@users.noreply.github.com>
2020-05-02 16:07:20 -0400

    rip p 5

--- P5/js/main.js
+++ P5/js/main.js
@@ -2,8 +2,10 @@ $(document).ready(function() {
 
     lever();
 
+
     $('.switch').click(function() {
 
+      spin()
       lever();
 
     });
@@ -28,13 +30,71 @@ function checkWin(number1,number2,number3){
 
 }
 
+function spin(){
+
+    var counter = 0;
+    var i = 0;
+
+
+    var slotArray1 = [".seven", ".square" , ".circle"];
+    var slotArray2 = [".seven2", ".square2" , ".circle2"];
+    var slotArray3 = [".seven3", ".square3" , ".circle3"];
+
+    for ( i in slotArray1){
+      addHidden(slotArray1);
+      $(slotArray1[i]).removeClass('hidden');
+      addHidden(slotArray1);
+      addHidden(slotArray2);
+      $(slotArray2[i]).removeClass('hidden');
+      addHidden(slotArray2);
+      addHidden(slotArray3);
+      $(slotArray3[i]).removeClass('hidden');
+      addHidden(slotArray3);
+      i++;
+      counter++;
+      if (counter > slotArray1.length){
+        i = 0;
+        counter = 0;
+        break;
+      }
+    }
+
+    for ( i in slotArray2){
+      addHidden(slotArray2);
+      $(slotArray2[i]).removeClass('hidden');
+      addHidden(slotArray2);
+      addHidden(slotArray3);
+      $(slotArray3[i]).removeClass('hidden');
+      addHidden(slotArray3);
+      i++;
+      counter++;
+      if (counter > slotArray2.length){
+        break;
+      }
+    }
+
+    for ( i in slotArray3){
+      addHidden(slotArray3);
+      $(slotArray3[i]).removeClass('hidden');
+      addHidden(slotArray3);
+      i++;
+      counter++;
+      if (counter > slotArray3.length){
+        break;
+      }
+    }
+
+  
+
+
+}
+
+
 function lever(){
       var number1 = Math.floor(Math.random() * 3);
       var number2 = Math.floor(Math.random() * 3);
       var number3 = Math.floor(Math.random() * 3);
-      console.log(number1);
-      console.log(number2);
-      console.log(number3);
+
 
         var slotArray1 = [".seven", ".square" , ".circle"];
         var slotArray2 = [".seven2", ".square2" , ".circle2"];
